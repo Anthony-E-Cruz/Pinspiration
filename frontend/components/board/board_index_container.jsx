d2b1f50d9afe179747ab3faa72f4d95352205033
[Anthony Cruz] <[anthonyecruz@gmail.com]>
2020-08-05 23:29:05 -0400

    board icon collage

--- frontend/components/board/board_index_container.jsx
+++ frontend/components/board/board_index_container.jsx
@@ -23,21 +23,85 @@ class ShowBoard extends React.Component {
     this.props.fetchPins();
   }
 
+  renderCollage(board) {
+      // console.log(board)
+    if (board[board.id]) {
+      let pins = board[board.id]
+      let pinUrls = Object.values(pins);
+      
+      if (pinUrls.length > 2) {
+        console.log(Object.values(pinUrls[0]));
+        let url = Object.values(pinUrls[0]);
+        let url2 = Object.values(pinUrls[1]);
+        let url3 = Object.values(pinUrls[2]);
+        return (
+          <div className="board-collage-container">
+            <img className="board-icon-large" src={url} />
+            <div>
+              <img className="board-icon-small-top" src={url2} />
+              <img className="board-icon-small-bottom" src={url3} />
+            </div>
+          </div>
+        );
+      } else if (pinUrls.length > 1) {
+        console.log(Object.values(pinUrls[0]));
+        let url = Object.values(pinUrls[0]);
+        let url2 = Object.values(pinUrls[1]);
+        // let url3 = Object.values(pinUrls[2]);
+        return (
+          <div className="board-collage-container">
+            <img className="board-icon-large" src={url} />
+            <div>
+              <img className="board-icon-small-top" src={url2} />
+
+              <div className="board-icon-small-bottom"></div>
+            </div>
+          </div>
+        );
+      } else if (pinUrls.length > 0) {
+        console.log(Object.values(pinUrls[0]));
+        let url = Object.values(pinUrls[0]);
+        // let url2 = Object.values(pinUrls[1]);
+        // let url3 = Object.values(pinUrls[2]);
+        return (
+          <div className="board-collage-container">
+            <img className="board-icon-large" src={url} />
+            <div>
+              {/* <img className="board-icon-small-top" src={url2} /> */}
+
+              <div className="board-icon-small-top"></div>
+              <div className="board-icon-small-bottom-placeholder"></div>
+            </div>
+          </div>
+        );
+      }
+    } else {
+      return (
+        <div className="board-collage-container">
+          <div className="board-icon-large" ></div>
+          <div>
+            <div className="board-icon-small-top"></div>
+            <div className="board-icon-small-bottom-placeholder"></div>
+          </div>
+        </div>
+      );
+    };
+  };
+
   render() {
     const { boards } = this.props;
     const allBoards = Object.values(boards);
-    // const board
-    // console.log(allBoards);
-    // console.log(allBoards[0]);
-    // console.log(allBoards[0].pins);
-    // if ()
     return (
       <div className="boards">
         {allBoards.map((board, idx) => (
           <Link key={idx} to={`/boards/${board.id}`}>
             <div key={idx} className="board-container">
               <div className="board">
-                {/* {console.log(board.pins.length)} */}
+                {this.renderCollage(board)}
+                {/* {const id = board.id} */}
+                {/* {console
+                  .log(Object.values(board[board.id]))
+                  .map((el) => el.photoUrl)} */}
                 {/* {if ((board.pins.length) > 0){
                   
                 }} */}
